43dd3f075ffb94d99adc20a6cead2ccfd0f26094
Jae Seong <qkrwotjd1445@naver.com>
2018-07-23 08:25:49 +0000

    link_to, image, whole of css, insert all files

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    // slideWidth: 450,
+    autoHover: true,
+    auto: true,
+    pager: true,
+    pause: 2000
+    });
+});
---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,14 @@
+class SitesController < ApplicationController
+    def index
+        render layout: 'index_layout'
+    end
+    
+    def club
+    end
+    
+    def club_content
+    end
+    
+    def competition
+    end
+end
